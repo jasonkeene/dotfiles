96cb886add701831daadeae50d806541a73face0
Jason Keene <jasonkeene@gmail.com>
2012-03-27 10:59:44 -0400

    allow create_symlinks.sh to get vars from env

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-DOTFILES=".vim .vimrc .gvimrc .bash_aliases .bash_profile .pythonrc.py"
-DIRNAME=`dirname $0`
-REPO_PATH=`cd $DIRNAME; pwd`
+if [ -n "${DOTFILES:-x}" ]; then
+    DOTFILES=".vim .vimrc .gvimrc .bash_aliases .bash_profile .pythonrc.py"
+done
+if [ -n "${DIRNAME:-x}" ]; then
+    DIRNAME=`dirname $0`
+done
+if [ -n "${REPO_PATH:-x}" ]; then
+    REPO_PATH=`cd $DIRNAME; pwd`
+done
 
 for DOTFILE in $DOTFILES; do
     if [ -e ~/$DOTFILE ]; then
