bd7c2925a012e0b8ed81d6ed7b3e901249d6c424
Jason Keene <jasonkeene@gmail.com>
2013-03-27 12:10:22 -0400

    refactor pythonrc to only run on 2.7 and fix tab completion to be able to run on < 2.7

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,29 +1,34 @@
-import os
-import atexit
-import readline
-import rlcompleter
+import sys
 
 
-HISTORY_PATH = os.path.expanduser("~/.pyhistory") # where to store history
-HISTORY_LENGTH = 2000 # how many of lines of history to store
-
-
-# enable tab completion
-readline.parse_and_bind("tab:complete")
-
-
-# save/restore history
-def save_history(path=HISTORY_PATH, length=HISTORY_LENGTH):
+def interactive_setup():
+    """Setup interactive interpreter with history and tab completion."""
+    import os
+    import atexit
     import readline
-    readline.set_history_length(length)
-    readline.write_history_file(path)
 
-if os.path.exists(HISTORY_PATH):
-    readline.read_history_file(HISTORY_PATH)
+    history_path = os.path.expanduser("~/.pyhistory")  # where to store history
+    history_length = 2000  # how many of lines of history to store
 
-atexit.register(save_history)
+    # enable tab completion
+    readline.parse_and_bind("tab:complete")
+
+    # restore history if it exists
+    if os.path.exists(history_path):
+        readline.read_history_file(history_path)
+    # save history ot exit
+
+    def save_history(path=history_path, length=history_length):
+        import readline
+        readline.set_history_length(length)
+        readline.write_history_file(path)
+    atexit.register(save_history)
+
+
+# only run on 2.7
+if sys.version_info[:2] == (2, 7):
+    interactive_setup()
 
 
 # do not litter!
-del os, atexit, readline, rlcompleter, save_history, HISTORY_PATH, \
-                                                                HISTORY_LENGTH
+del sys, interactive_setup
